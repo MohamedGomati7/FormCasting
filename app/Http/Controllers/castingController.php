cb8a63a97368ff96cbb117eebec2b39197679273
MohamedGomati7 <MohamedGomati7@github.com>
2020-07-15 14:26:59 +0200

    Route , Controller

---
+++ app/Http/Controllers/castingController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class castingController extends Controller
+{
+    public function casting{
+        
+        return view ('/casting');
+    }
+}
